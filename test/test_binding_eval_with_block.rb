07ce2e2fdf8a03ce89165e367daf67e6189f8caa
Tadashi Saito <tad.a.digger@gmail.com>
2014-11-22 10:13:13 +0900

    loosen checking

--- test/test_binding_eval_with_block.rb
+++ test/test_binding_eval_with_block.rb
@@ -2,7 +2,7 @@ require 'minitest_helper'
 
 class TestBindingEvalWithBlock < MiniTest::Test
   def test_version_number_validity
-    assert_match /^\d\.\d\.\d$/, Binding::EvalWithBlock::VERSION
+    assert_match /^\d+\.\d+\.\d+/, Binding::EvalWithBlock::VERSION
   end
 
   def test_internal_alias_method_set
