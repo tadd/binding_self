5f4b85d5ffece3f4789d75cd17c922720437407e
Tadashi Saito <tad.a.digger@gmail.com>
2014-11-22 10:09:23 +0900

    define alias

--- ext/binding_eval_with_block/binding_eval_with_block.c
+++ ext/binding_eval_with_block/binding_eval_with_block.c
@@ -3,4 +3,5 @@
 void
 Init_binding_eval_with_block(void)
 {
+    rb_define_alias(rb_cBinding, "_eval_original", "eval");
 }
--- test/test_binding_eval_with_block.rb
+++ test/test_binding_eval_with_block.rb
@@ -1,7 +1,11 @@
 require 'minitest_helper'
 
-class TestBindingEvalWithBlock < MiniTest::Unit::TestCase
+class TestBindingEvalWithBlock < MiniTest::Test
   def test_version_number_validity
     assert_match /^\d\.\d\.\d$/, Binding::EvalWithBlock::VERSION
+  end
+
+  def test_internal_alias_method_set
+    assert_includes Binding.instance_methods(false), :_eval_original
   end
 end
