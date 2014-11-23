daa446058b2bbf5aa70d01975fcb349525d80789
Tadashi Saito <tad.a.digger@gmail.com>
2014-11-23 15:25:50 +0900

    add more tests

--- test/test_binding_eval_with_block.rb
+++ test/test_binding_eval_with_block.rb
@@ -8,4 +8,13 @@ class TestBindingEvalWithBlock < MiniTest::Test
   def test_internal_alias_method_set
     assert_includes Binding.instance_methods(false), :_eval_original
   end
+
+  def test_nothing_raised_with_block_without_any_arguments
+    binding.eval {}
+    assert true
+  end
+
+  def test_compatible_with_string_eval
+    assert_equal 1, binding.eval('1')
+  end
 end
