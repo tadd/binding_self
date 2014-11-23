2a65882fbb3712be0a3224e5ef4fa8390845335e
Tadashi Saito <tad.a.digger@gmail.com>
2014-11-23 16:03:49 +0900

    implemented... (really?

--- ext/binding_eval_with_block/binding_eval_with_block.c
+++ ext/binding_eval_with_block/binding_eval_with_block.c
@@ -1,7 +1,80 @@
 #include "binding_eval_with_block.h"
 
+/*
+ * copy from vm_core.h
+ */
+
+#define GetCoreDataFromValue(obj, type, ptr) do { \
+    (ptr) = (type*)DATA_PTR(obj); \
+} while (0)
+
+typedef struct rb_block_struct {
+    VALUE self;			/* share with method frame if it's only block */
+    VALUE klass;		/* share with method frame if it's only block */
+    VALUE *ep;			/* share with method frame if it's only block */
+    void *iseq; // changed
+    VALUE proc;
+} rb_block_t;
+
+#define GetEnvPtr(obj, ptr) \
+  GetCoreDataFromValue((obj), rb_env_t, (ptr))
+
+typedef struct {
+    VALUE *env;
+    int env_size;
+    int local_size;
+    VALUE prev_envval;		/* for GC mark */
+    rb_block_t block;
+} rb_env_t;
+
+#define GetBindingPtr(obj, ptr) \
+  GetCoreDataFromValue((obj), rb_binding_t, (ptr))
+
+typedef struct {
+    VALUE env;
+    VALUE path;
+    VALUE blockprocval;	/* for GC mark */
+    unsigned short first_lineno;
+} rb_binding_t;
+
+/*
+ * copy ends here
+ */
+
+static VALUE
+get_self(VALUE bindingval)
+{
+    rb_binding_t *binding;
+    rb_env_t *env;
+
+    GetBindingPtr(bindingval, binding);
+    GetEnvPtr(binding->env, env);
+    return env->block.self;
+}
+
+static VALUE
+block_eval(VALUE binding)
+{
+    const VALUE self = get_self(binding);
+
+    return rb_obj_instance_eval(0, NULL, self);
+}
+
+
+static VALUE
+bind_eval_with_block(int argc, VALUE *argv, VALUE binding)
+{
+    if (rb_block_given_p()) {
+	rb_check_arity(argc, 0, 0);
+	return block_eval(binding);
+    } else
+	return rb_funcallv(rb_cBinding, rb_intern("eval"), argc, argv);
+}
+
+
 void
 Init_binding_eval_with_block(void)
 {
     rb_define_alias(rb_cBinding, "_eval_original", "eval");
+    rb_define_method(rb_cBinding, "eval", bind_eval_with_block, -1);
 }
--- test/test_binding_eval_with_block.rb
+++ test/test_binding_eval_with_block.rb
@@ -1,5 +1,15 @@
 require 'minitest_helper'
 
+class C
+  def initialize(x)
+    @x = x
+  end
+
+  def binding
+    ::Kernel.binding
+  end
+end
+
 class TestBindingEvalWithBlock < MiniTest::Test
   def test_version_number_validity
     assert_match /^\d+\.\d+\.\d+/, Binding::EvalWithBlock::VERSION
@@ -16,5 +26,15 @@ class TestBindingEvalWithBlock < MiniTest::Test
 
   def test_compatible_with_string_eval
     assert_equal 1, binding.eval('1')
+  end
+
+  def test_block_eval
+    a = 1
+    assert_equal 1, binding.eval { a }
+  end
+
+  def test_block_eval_more
+    c = C.new(42)
+    assert_equal 42, c.binding.eval { @x }
   end
 end
